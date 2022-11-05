8d6039967320d8500bbd44604a93f899394969a4
ingChesterW <109614833+IngChesterW@users.noreply.github.com>
2022-11-04 21:18:20 -0400

    Add files via upload

---
+++ auxiliaturas-INF110-main/pascal/numerosNaturales/numNat2.pas
@@ -0,0 +1,13 @@
+//preguntar si a es divisible entre b 
+program numNat2;
+uses crt;
+  var num1, num2 : integer;
+begin 
+readln(num1);
+readln(num2);
+if (num1  mod num2 ) =  0 then
+    begin
+     writeln('a es divisible a b');
+    end
+
+end.
